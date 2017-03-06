0e9394274f205b75ce94a3928a7fb7e829bea6f1
unknown <contact@manxuantien.com>
2017-03-06 13:31:28 +0700

    First commit

---
+++ application/logs/log-2017-03-01.php
@@ -0,0 +1,108 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2017-03-01 07:02:56 --> Config Class Initialized
+INFO - 2017-03-01 07:02:56 --> Hooks Class Initialized
+DEBUG - 2017-03-01 07:02:56 --> UTF-8 Support Enabled
+INFO - 2017-03-01 07:02:56 --> Utf8 Class Initialized
+INFO - 2017-03-01 07:02:56 --> URI Class Initialized
+DEBUG - 2017-03-01 07:02:56 --> No URI present. Default controller set.
+INFO - 2017-03-01 07:02:56 --> Router Class Initialized
+INFO - 2017-03-01 07:02:56 --> Output Class Initialized
+INFO - 2017-03-01 07:02:56 --> Security Class Initialized
+DEBUG - 2017-03-01 07:02:56 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-01 07:02:56 --> Input Class Initialized
+INFO - 2017-03-01 07:02:56 --> Language Class Initialized
+INFO - 2017-03-01 07:02:56 --> Language Class Initialized
+INFO - 2017-03-01 07:02:56 --> Config Class Initialized
+INFO - 2017-03-01 07:02:56 --> Loader Class Initialized
+INFO - 2017-03-01 07:02:56 --> Controller Class Initialized
+DEBUG - 2017-03-01 07:02:56 --> Welcome MX_Controller Initialized
+DEBUG - 2017-03-01 07:02:56 --> File loaded: C:\xampp\htdocs\Dmobile.vn\application\modules/Welcome/views/welcome_message.php
+INFO - 2017-03-01 07:02:56 --> Final output sent to browser
+DEBUG - 2017-03-01 07:02:56 --> Total execution time: 0.0740
+INFO - 2017-03-01 07:46:57 --> Config Class Initialized
+INFO - 2017-03-01 07:46:57 --> Hooks Class Initialized
+DEBUG - 2017-03-01 07:46:57 --> UTF-8 Support Enabled
+INFO - 2017-03-01 07:46:57 --> Utf8 Class Initialized
+INFO - 2017-03-01 07:46:57 --> URI Class Initialized
+INFO - 2017-03-01 07:46:57 --> Router Class Initialized
+INFO - 2017-03-01 07:46:57 --> Output Class Initialized
+INFO - 2017-03-01 07:46:57 --> Security Class Initialized
+DEBUG - 2017-03-01 07:46:57 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-01 07:46:57 --> Input Class Initialized
+INFO - 2017-03-01 07:46:57 --> Language Class Initialized
+INFO - 2017-03-01 07:46:57 --> Language Class Initialized
+INFO - 2017-03-01 07:46:57 --> Config Class Initialized
+INFO - 2017-03-01 07:46:57 --> Loader Class Initialized
+INFO - 2017-03-01 07:46:57 --> Controller Class Initialized
+DEBUG - 2017-03-01 07:46:57 --> Welcome MX_Controller Initialized
+DEBUG - 2017-03-01 07:46:57 --> File loaded: C:\xampp\htdocs\Dmobile.vn\application\modules/welcome/views/welcome_message.php
+INFO - 2017-03-01 07:46:57 --> Final output sent to browser
+DEBUG - 2017-03-01 07:46:57 --> Total execution time: 0.0660
+INFO - 2017-03-01 07:53:56 --> Config Class Initialized
+INFO - 2017-03-01 07:53:56 --> Hooks Class Initialized
+DEBUG - 2017-03-01 07:53:56 --> UTF-8 Support Enabled
+INFO - 2017-03-01 07:53:56 --> Utf8 Class Initialized
+INFO - 2017-03-01 07:53:56 --> URI Class Initialized
+INFO - 2017-03-01 07:53:56 --> Router Class Initialized
+INFO - 2017-03-01 07:53:56 --> Output Class Initialized
+INFO - 2017-03-01 07:53:56 --> Security Class Initialized
+DEBUG - 2017-03-01 07:53:56 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-01 07:53:56 --> Input Class Initialized
+INFO - 2017-03-01 07:53:56 --> Language Class Initialized
+INFO - 2017-03-01 07:53:56 --> Language Class Initialized
+INFO - 2017-03-01 07:53:56 --> Config Class Initialized
+INFO - 2017-03-01 07:53:56 --> Loader Class Initialized
+INFO - 2017-03-01 07:53:56 --> Controller Class Initialized
+DEBUG - 2017-03-01 07:53:56 --> Welcome MX_Controller Initialized
+DEBUG - 2017-03-01 07:53:56 --> File loaded: C:\xampp\htdocs\Dmobile.vn\application\modules/welcome/views/welcome_message.php
+INFO - 2017-03-01 07:53:56 --> Final output sent to browser
+DEBUG - 2017-03-01 07:53:56 --> Total execution time: 0.0760
+INFO - 2017-03-01 07:54:55 --> Config Class Initialized
+INFO - 2017-03-01 07:54:55 --> Hooks Class Initialized
+DEBUG - 2017-03-01 07:54:56 --> UTF-8 Support Enabled
+INFO - 2017-03-01 07:54:56 --> Utf8 Class Initialized
+INFO - 2017-03-01 07:54:56 --> URI Class Initialized
+DEBUG - 2017-03-01 07:54:56 --> No URI present. Default controller set.
+INFO - 2017-03-01 07:54:56 --> Router Class Initialized
+INFO - 2017-03-01 07:54:56 --> Output Class Initialized
+INFO - 2017-03-01 07:54:56 --> Security Class Initialized
+DEBUG - 2017-03-01 07:54:56 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-01 07:54:56 --> Input Class Initialized
+INFO - 2017-03-01 07:54:56 --> Language Class Initialized
+INFO - 2017-03-01 07:54:56 --> Language Class Initialized
+INFO - 2017-03-01 07:54:56 --> Config Class Initialized
+INFO - 2017-03-01 07:54:56 --> Loader Class Initialized
+INFO - 2017-03-01 07:54:56 --> Controller Class Initialized
+DEBUG - 2017-03-01 07:54:56 --> Home MX_Controller Initialized
+DEBUG - 2017-03-01 07:54:56 --> Template class Initialized
+INFO - 2017-03-01 07:54:56 --> Helper loaded: url_helper
+INFO - 2017-03-01 07:54:56 --> Model Class Initialized
+DEBUG - 2017-03-01 07:54:56 --> File loaded: C:\xampp\htdocs\Dmobile.vn\application\modules/home/models/M_home.php
+INFO - 2017-03-01 07:54:56 --> Model Class Initialized
+ERROR - 2017-03-01 07:54:56 --> Severity: Runtime Notice --> Only variables should be assigned by reference C:\xampp\htdocs\Dmobile.vn\application\libraries\Template.php 196
+INFO - 2017-03-01 07:54:56 --> Parser Class Initialized
+INFO - 2017-03-01 07:54:56 --> Config Class Initialized
+INFO - 2017-03-01 07:54:56 --> Hooks Class Initialized
+DEBUG - 2017-03-01 07:54:56 --> UTF-8 Support Enabled
+INFO - 2017-03-01 07:54:56 --> Utf8 Class Initialized
+INFO - 2017-03-01 07:54:56 --> URI Class Initialized
+DEBUG - 2017-03-01 07:54:56 --> No URI present. Default controller set.
+INFO - 2017-03-01 07:54:56 --> Router Class Initialized
+INFO - 2017-03-01 07:54:56 --> Output Class Initialized
+INFO - 2017-03-01 07:54:56 --> Security Class Initialized
+DEBUG - 2017-03-01 07:54:56 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-01 07:54:56 --> Input Class Initialized
+INFO - 2017-03-01 07:54:56 --> Language Class Initialized
+INFO - 2017-03-01 07:54:56 --> Language Class Initialized
+INFO - 2017-03-01 07:54:56 --> Config Class Initialized
+INFO - 2017-03-01 07:54:56 --> Loader Class Initialized
+INFO - 2017-03-01 07:54:56 --> Controller Class Initialized
+DEBUG - 2017-03-01 07:54:56 --> Home MX_Controller Initialized
+DEBUG - 2017-03-01 07:54:56 --> Template class Initialized
+INFO - 2017-03-01 07:54:56 --> Helper loaded: url_helper
+INFO - 2017-03-01 07:54:56 --> Model Class Initialized
+DEBUG - 2017-03-01 07:54:56 --> File loaded: C:\xampp\htdocs\Dmobile.vn\application\modules/home/models/M_home.php
+INFO - 2017-03-01 07:54:56 --> Model Class Initialized
+ERROR - 2017-03-01 07:54:56 --> Severity: Runtime Notice --> Only variables should be assigned by reference C:\xampp\htdocs\Dmobile.vn\application\libraries\Template.php 196
+INFO - 2017-03-01 07:54:56 --> Parser Class Initialized
---
+++ application/views/partials/v_header.php
@@ -0,0 +1,28 @@
+<div class="header">
+    <div id="top-header">
+        <div id="logo">
+            <a href="//dmobile.vn">
+                <img alt="dmobile.vn" src='<?php echo base_url()."/assets/images/logo.png"; ?>'>
+            </a>
+        </div>
+        <h5 id="hot-line">
+            HOTLINE: 0985180192
+            <span>
+                <br>
+                Số 22 - Ngõ 251 - Cầu Giấy - Hà Nội
+            </span>
+        </h5>
+    </div>
+    <div id="main-menu">
+        <ul>
+            <li><a href="/">Trang Chủ</a></li>
+            <li><a href="/">Giới Thiệu</a></li>
+            <li><a href="/">Sản Phẩm</a></li>
+            <li><a href="/">Dịch Vụ</a></li>
+            <li><a href="/">Công Nghệ</a></li>
+            <li><a href="/">Bảo Hành</a></li>
+            <li><a href="/">Tin tức</a></li>
+            <li><a href="/">Bản Đồ</a></li>
+        </ul>
+    </div>
+</div>
